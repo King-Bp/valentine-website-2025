988a4f14d7459bfd48a87fe7c7bea4ce36e23892
King-Bp <praiseb036@gmail.com>
2026-02-08 16:36:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        Diamonds: ['❤️', '💎', '💝', '💗', '💓'],  // Heart emojis
+        hearts: ['❤️', '💎', '💝', '💗', '💓'],  // Heart emojis
         bears: ['🧸', '🐻']                       // Cute bear emojis
     },
 
