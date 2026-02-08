c7e74025ebd17c064c69e7f3cb2b2a90a6c0a87b
King-Bp <praiseb036@gmail.com>
2026-02-08 16:59:13 +0200

    Update config.js

--- config.js
+++ config.js
@@ -22,10 +22,9 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me?",                                    // First interaction
+            text: "It's about time things stepped up a bit yes?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
         },
         second: {
             text: "How much do you love me? falling in love with you, and haven't wanted to fall more as I do now.",                          // For the love meter
